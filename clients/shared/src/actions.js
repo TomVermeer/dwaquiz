c0502026a09bfc7a5a601cfb64c9dafb60b170d2
Erik Knaake <e.knaake@student.han.nl>
2020-10-13 13:52:53 +0200

    POC local shared module for react components

---
+++ clients/shared/src/actions.js
@@ -0,0 +1,5 @@
+export const SharedActions = Object.freeze({
+    NEXT_QUESTION: 0,
+    NEXT_ROUND: 1,
+    NIGHT_END: 2
+});
---
+++ clients/shared/src/components/GameProgress.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const GameProgress = (props) => (
+    <h1>Test vanuit shared</h1>
+);

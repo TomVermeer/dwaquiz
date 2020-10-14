b41eccc8d710572ee2d942dcbbbd13e30a25d395
Erik Knaake <e.knaake@student.han.nl>
2020-10-14 15:22:13 +0200

    create quiznight

--- server/route-handlers/quizNight.js
+++ server/route-handlers/quizNight.js
@@ -1,12 +1,9 @@
 const { QuizNight } = require("../models");
 
+// https://stackoverflow.com/a/58509734/7736404
 const generateQuizPin = (length) => Math.floor(Math.random() * (9 * Math.pow(10, length - 1))) + Math.pow(10, length - 1);
 
-const doesPinAlreadExist = async (quizPin) => {
-    const pinInDb = await QuizNight.findOne({_id: quizPin}).exec();
-    console.log('pin in db: ', pinInDb, ' generated one: ', quizPin);
-    return pinInDb != null;
-}
+const doesPinAlreadExist = async (quizPin) => await QuizNight.findOne({_id: quizPin}).exec() != null;
 
 const getNewQuizPin = async (length = 6) => {
     let quizPin;
@@ -19,11 +16,22 @@ const getNewQuizPin = async (length = 6) => {
 }
 
 const createQuizNightHandler = async (req, res) => {
-    const pin = await getNewQuizPin();
-    console.log('final pin: ', pin)
-    res.json({
-        quizPin: pin
-    }); // TODO generate pin and save to db
+    try {
+        const pin = await getNewQuizPin();
+        const quizNight = new QuizNight({
+            _id: pin,
+            teams: [],
+            rounds: []
+        });
+        await quizNight.save();
+
+        res.json({
+            quizPin: quizNight._id
+        });
+    } catch (e) {
+        console.log('ERROR: ', e);
+        throw e;
+    }
 };
 
 module.exports = { createQuizNightHandler };

30fc323a9d06462360dd89cdd982be96c9dad9c8
Tom desktop <vermeertom0@gmail.com>
2020-10-14 11:48:17 +0200

    added boostrap and sass to clients

--- clients/team/src/index.js
+++ clients/team/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
@@ -15,6 +15,7 @@ ReactDOM.render(
       <App />
     </Provider>
   </React.StrictMode>,
+  document.getElementById('root')
 );
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

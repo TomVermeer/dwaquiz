2de1d71679d1643b694fbf6d2c9a95684d1c6a67
Erik Knaake <e.knaake@student.han.nl>
2020-10-14 12:14:00 +0200

    websocket infrastructure + fix shared module to work better with multiple files

--- clients/scoreboard/src/index.js
+++ clients/scoreboard/src/index.js
@@ -6,8 +6,10 @@ import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { rootReducer } from './reducers/rootReducer';
+import { setStore } from 'shared/store';
 
 const store = createStore(rootReducer);
+setStore(store);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,8 +17,8 @@ ReactDOM.render(
       <App />
     </Provider>
   </React.StrictMode>,
+  document.getElementById('root')
 );
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA

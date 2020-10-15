e68162436b864b8080de69715d275987fe469651
Erik Knaake <e.knaake@student.han.nl>
2020-10-15 10:39:51 +0200

    dont execute happy path on http error

--- clients/team/src/reducers/quizProgress/quizProgressActionCreators.js
+++ clients/team/src/reducers/quizProgress/quizProgressActionCreators.js
@@ -1,22 +1,23 @@
-import { Actions } from '../../actions';
-import { Pages } from '../../pages';
+import {Actions} from '../../actions';
+import {Pages} from '../../pages';
 import {post} from "shared/fetchHelpers";
 import {isErrorResponse} from "shared/fetchHelpers";
 
 const onTeamApply = (teamName, quizPin) => {
     return {
         type: Actions.ON_TEAM_APPLY,
-        payload: { teamName, quizPin }
+        payload: {teamName, quizPin}
     }
 };
 
 export const applyTeam = (teamName, quizPin, history) => dispatch => {
     post(`quiz-nights/${quizPin}/team-applications`, {teamName})
         .then((response) => {
-            if(isErrorResponse(response)) {
+            if (isErrorResponse(response)) {
                 // TODO: handle errors
+            } else {
+                dispatch(onTeamApply(teamName, quizPin));
+                history.push(Pages.WAIT_FOR_APPROVAL);
             }
-            dispatch(onTeamApply(teamName, quizPin));
-            history.push(Pages.WAIT_FOR_APPROVAL);
         });
 };

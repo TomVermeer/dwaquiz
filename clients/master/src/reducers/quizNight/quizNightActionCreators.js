4dbb5b787e0b99709dd47e6c6307efda133b4b5e
Erik Knaake <e.knaake@student.han.nl>
2020-10-15 10:38:16 +0200

    fetch helpers

--- clients/master/src/reducers/quizNight/quizNightActionCreators.js
+++ clients/master/src/reducers/quizNight/quizNightActionCreators.js
@@ -1,19 +1,14 @@
-import { API_BASE_URL } from "shared/constants"
 import { getWebsocket } from "shared/websocket";
 import { Actions } from "../../actions";
+import {postAndParse} from "shared/fetchHelpers";
 
 export const openQuizNight = () => (dispatch) => {
-    fetch(API_BASE_URL + 'quiz-nights',
-        {
-            method: 'POST',
-            credentials: 'include'
-        })
-        .then(data => data.json())
+    postAndParse('quiz-nights')
         .then(json => {
             getWebsocket(); // Open websocket to listen to teams that apply
             dispatch(onOpenQuizNight(json));
         });
-}
+};
 
 const onOpenQuizNight = (response) => {
     return { type: Actions.ON_OPEN_QUIZ_NIGHT, payload: response.quizPin }
--- clients/team/src/reducers/quizProgress/quizProgressActionCreators.js
+++ clients/team/src/reducers/quizProgress/quizProgressActionCreators.js
@@ -1,6 +1,7 @@
-import { API_BASE_URL } from 'shared/constants';
 import { Actions } from '../../actions';
 import { Pages } from '../../pages';
+import {post} from "shared/fetchHelpers";
+import {isErrorResponse} from "shared/fetchHelpers";
 
 const onTeamApply = (teamName, quizPin) => {
     return {
@@ -10,16 +11,11 @@ const onTeamApply = (teamName, quizPin) => {
 };
 
 export const applyTeam = (teamName, quizPin, history) => dispatch => {
-    fetch(API_BASE_URL + 'quiz-nights/' + quizPin + '/team-applications',
-        {
-            method: 'POST',
-            body: JSON.stringify({ teamName }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
+    post(`quiz-nights/${quizPin}/team-applications`, {teamName})
         .then((response) => {
-            // TODO check against 404 quiznight not found
+            if(isErrorResponse(response)) {
+                // TODO: handle errors
+            }
             dispatch(onTeamApply(teamName, quizPin));
             history.push(Pages.WAIT_FOR_APPROVAL);
         });

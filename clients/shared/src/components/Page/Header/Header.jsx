b4c64414b568b1aac05dabc12db2ac2207cd170d
Erik Knaake <e.knaake@student.han.nl>
2020-10-16 16:15:36 +0200

    fix title folder misspelling

--- clients/shared/src/components/Page/Header/Header.jsx
+++ clients/shared/src/components/Page/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './header.scss';
 import {QuizProgress} from "./QuizProgress/QuizProgress";
-import {Title} from "./title/Title";
+import {Title} from "./Title/Title";
 
 export const Header = (props) => {
     return (

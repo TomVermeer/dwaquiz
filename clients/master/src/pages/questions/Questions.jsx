fe09490d205932d4dd90ca7fe74b696769cb0f6b
Erik Knaake <e.knaake@student.han.nl>
2020-10-23 11:47:04 +0200

    initial styling questions

--- clients/master/src/pages/questions/Questions.jsx
+++ clients/master/src/pages/questions/Questions.jsx
@@ -3,22 +3,30 @@ import './questions.scss';
 import {useSelector, useDispatch} from 'react-redux';
 import {fetchSuggestedQuestions} from "../../reducers/rootActionCreators";
 import {useTitle} from "../../effects/useTitle";
+import {Panel} from "../../components/Panel/Panel";
 
 export const Questions = (props) => {
 
-  const {quizPin, roundNumber} = useSelector(state => state.shared.quizProgress);
-  const suggestedQuestions = useSelector(state => state.root.suggestedQuestions);
-  const dispatch = useDispatch();
+    const {quizPin, roundNumber} = useSelector(state => state.shared.quizProgress);
+    const suggestedQuestions = useSelector(state => state.root.suggestedQuestions);
+    const dispatch = useDispatch();
 
-  useTitle('Kiez vraag');
+    useTitle('Kiez vraag');
 
-  useEffect(() => {
-      dispatch(fetchSuggestedQuestions(quizPin, roundNumber, 0, 20));
-  }, [quizPin, roundNumber, dispatch]);
+    useEffect(() => {
+        dispatch(fetchSuggestedQuestions(quizPin, roundNumber, 0, 20));
+    }, [quizPin, roundNumber, dispatch]);
 
-  return (
-      <div>
-          {suggestedQuestions.map(x => <p key={x.question}>{x.category + ' ' + x.question}</p>)}
-      </div>
-  );
+    return (
+        <div className="question-panel">
+            <Panel
+                header={<div className="space-between"><div>Categorie</div><div>Vraag</div></div>}
+                rows={suggestedQuestions.map(x =>
+                    <div className="space-between">
+                        <div>{x.category}</div>
+                        <div>{x.question}</div>
+                    </div>)}>
+            </Panel>
+        </div>
+    );
 };

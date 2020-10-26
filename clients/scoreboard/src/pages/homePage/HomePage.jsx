2be4195eaac3c2d2baa3ee9a7cb1fce1b4dba3e5
Tom desktop <vermeertom0@gmail.com>
2020-10-26 11:43:41 +0100

    import fix

--- clients/scoreboard/src/pages/homePage/HomePage.jsx
+++ clients/scoreboard/src/pages/homePage/HomePage.jsx
@@ -3,7 +3,7 @@ import { useDispatch } from "react-redux";
 import { useState } from 'react'
 import { Button, Container, Form, Navbar, Col } from "react-bootstrap";
 import {Footer} from "../../components"
-import {openQuizNight} from "../../reducers/quizProgressActionCreators"
+import {openQuizNight} from "../../reducers/mainActionCreators"
 
 import "../homePage/homepage.scss";
 import { useHistory } from "react-router";

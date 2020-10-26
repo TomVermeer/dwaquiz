1c61c8c23a8c2674df3644e4cb37b81d07b75f67
Erik Knaake <e.knaake@student.han.nl>
2020-10-26 10:33:39 +0100

    reference Question and QuizNight from Questionings, reference Questioning from QuizNight.round.questionings

--- server/models.js
+++ server/models.js
@@ -1,11 +1,20 @@
 const mongoose = require('mongoose');
 
+const questionSchema = mongoose.Schema({
+    question: {type: String, required: true},
+    category: { type: String, required: true },
+    answer: { type: String, required: true },
+    orderNumber: {type: Number, required: true}
+});
+
 const questioningsSchema = new mongoose.Schema({
     teamName: { type: String, required: true },
-    question: { type: String, required: true },
-    questionId: {type: String, required: true},
+    question: { type: mongoose.Schema.Types.ObjectId, ref: 'Question', require: true},
     answer: String,
-    isCorrect: { type: Boolean, default: false }
+    isCorrect: { type: Boolean, default: false },
+    questionNumber: {type: Number, required: true},
+    roundNumber: {type: Number, required: true},
+    quizPin: {type: Number, ref: 'QuizNight', required: true}
 });
 
 const teamSchema = mongoose.Schema({
@@ -17,7 +26,7 @@ const teamSchema = mongoose.Schema({
 const roundSchema = mongoose.Schema({
     chosenCategories: { type: [String], required: true },
     roundNumber: { type: Number, default: 1 },
-    questionings: { type: [questioningsSchema], required: true },
+    questionings: { type: [{type: mongoose.Schema.Types.ObjectId, ref: 'Questioning'}], required: true },
 });
 
 const quizNightSchema = mongoose.Schema({
@@ -28,14 +37,8 @@ const quizNightSchema = mongoose.Schema({
     isOpenForApplication: {type: Boolean, default: true}
 });
 
-const questionSchema = mongoose.Schema({
-    question: {type: String, required: true},
-    category: { type: String, required: true },
-    answer: { type: String, required: true },
-    orderNumber: {type: Number, required: true}
-});
-
-const QuizNight = mongoose.model('QuizNight', quizNightSchema);
 const Question = mongoose.model('Question', questionSchema);
+const QuizNight = mongoose.model('QuizNight', quizNightSchema);
+const Questioning = mongoose.model('Questioning', questioningsSchema);
 
-module.exports = { quizNightSchema, questionSchema, QuizNight, Question };
+module.exports = { quizNightSchema, questionSchema, questioningsSchema, QuizNight, Question, Questioning };

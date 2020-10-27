7f1d7721cb559fcb2e6ada6b5146ba8b99b7c289
Erik Knaake <e.knaake@student.han.nl>
2020-10-27 16:57:33 +0100

    remove unused function

--- clients/scoreboard/src/reducers/mainActionCreators.js
+++ clients/scoreboard/src/reducers/mainActionCreators.js
@@ -5,10 +5,6 @@ export const openQuizNight = (quizpin) => (dispatch) => {
     startWebsocket(quizpin); // TODO: refactor this to hooks with routers
 };
 
-const onOpenQuizNight = (response) => {
-    return {type: SharedActions.SET_QUIZ_PIN, payload: response.quizPin};
-};
-
 export const setParticipatingTeams = (teamNames) => {
     return {
         type: SharedActions.ON_TEAM_APPROVAL, payload: teamNames

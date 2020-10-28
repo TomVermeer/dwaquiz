1795dee194aecf8eeb75813280303f3b584937e9
Tom desktop <vermeertom0@gmail.com>
2020-10-28 09:38:43 +0100

    fixed duplicate funtion

--- clients/scoreboard/src/reducers/mainActionCreators.js
+++ clients/scoreboard/src/reducers/mainActionCreators.js
@@ -38,10 +38,6 @@ export const fetchQuestion = (quizPin, roundNumber, questionNumber) => dispatch 
   })
 }
 
-export const openQuizNight = (quizpin) => (dispatch) => {
-    startWebsocket(quizpin); // TODO: refactor this to hooks with routers
-};
-
 export const setParticipatingTeams = (teamNames) => {
     return {
         type: SharedActions.ON_TEAM_APPROVAL, payload: teamNames

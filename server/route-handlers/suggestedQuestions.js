f5cfa3534adc5f736c65709aa01674fe5aa031d3
Erik Knaake <e.knaake@student.han.nl>
2020-11-01 16:09:08 +0100

    dont fetch question that is already asked

--- server/route-handlers/suggestedQuestions.js
+++ server/route-handlers/suggestedQuestions.js
@@ -3,6 +3,7 @@ const {Question} = require('../persistence/models');
 const getSuggestedQuestionsHandler = async (req, res) => {
     try {
         const suggestedQuestions = await Question.findSuggestedQuestionsForQuizNight(req.quizPin, req.round, req.query.offset, req.query.limit);
+        console.log({suggestedQuestions});
         res.json(suggestedQuestions);
     } catch (e) {
         throw e;

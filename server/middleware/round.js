7c8284059a1e141d2012ee76f04975bbc8022063
Tom desktop <vermeertom0@gmail.com>
2020-11-04 09:37:48 +0100

    added retrieval of current quiznight

--- server/middleware/round.js
+++ server/middleware/round.js
@@ -4,7 +4,10 @@ const HttpErrors = require('../httpErrors');
 const guardRoundNumberExists = async (req, res, next) => {
     try {
         req.round = Number(req.params.round);
-        dbRound = await QuizNight.findRound(req.params.round)
+        
+        currentQuizNight = await QuizNight.findById(req.params.quizPin)
+        dbRound = await currentQuizNight.findRound(req.params.round)
+
         if(!req.round || !dbRound) {
             res.sendError(HttpErrors.NO_ROUND_NUMBER_FOUND);
         } else {
